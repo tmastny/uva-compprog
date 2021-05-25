7a75cd969d566246058103040bae96e1e72be8d1
Timothy Mastny <tim.mastny@gmail.com>
2021-05-24 21:32:48 -0500

    save

--- uva-10258/main.cpp
+++ uva-10258/main.cpp
@@ -22,14 +22,29 @@ class Contestant {
     int correct = 0;
     int penalty = 0;
     int id;
+    bool attempted = false;
 
     Contestant(int id);
     void update(int problem, int time, string outcome);
 };
 
+bool operator<(const Contestant &c1, const Contestant &c2) {
+  if (c1.correct < c2.correct) return true;
+  if (c1.correct > c2.correct) return false;
+
+  if (c1.penalty < c1.penalty) return true;
+  if (c1.penalty > c1.penalty) return false;
+
+  if (c1.id < c1.id) return true;
+  if (c1.id > c1.id) return false;
+
+  return false;
+}
+
 Contestant::Contestant(int id) : id(id) {}
 
 void Contestant::update(int problem, int time, string outcome) {
+  attempted = true;
 
   if (outcome == "I") {
     problem_penalty[problem] = 20;
@@ -38,7 +53,7 @@ void Contestant::update(int problem, int time, string outcome) {
 
   if (outcome == "C") {
     correct++;
-    penalty = problem_penalty[problem] + time;
+    penalty += problem_penalty[problem] + time;
     return;
   }
 }
@@ -59,8 +74,6 @@ int main() {
 
   while (cases--) {
 
-    cout << cases << endl;
-
     int contestant, problem, time;
     string outcome;
 
@@ -75,9 +88,24 @@ int main() {
     while (!line.empty()) {
       istringstream ss(line);
       ss >> contestant >> problem >> time >> outcome;
-      cout << outcome << endl;
+
+      scores[contestant]->update(problem, time, outcome);
+
 
       getline(cin, line);
+    }
+
+    vector<Contestant*> rank;
+    for (auto x : scores) {
+      if (x.second->attempted) {
+        rank.push_back(x.second);
+      }
+    }
+
+    sort(rank.begin(), rank.end());
+
+    for (auto c : rank) {
+      cout << c->id << " " << c->correct << " " << c->penalty << endl;
     }
 
 
