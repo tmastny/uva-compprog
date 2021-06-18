0910082fc21e8aebc56204b4265b13651867c950
Timothy Mastny <tim.mastny@gmail.com>
2021-06-18 14:48:12 -0500

    save

--- algorithms-dpv/rsa/main.cpp
+++ algorithms-dpv/rsa/main.cpp
@@ -26,6 +26,17 @@ int inverse(int a, int N) {
     return (N + extended_euclidean(a, N).x) % N;
 }
 
+int modexp(int x, int y, int N) {
+    if (y == 0) return 1;
+    int z = modexp(x, y / 2, N);
+
+    if (y % 2 == 0) {
+        return z * z % N;
+    }
+
+    return x * z * z % N;
+}
+
 int main() {
     auto e = extended_euclidean(25, 11);
 
@@ -33,4 +44,35 @@ int main() {
 
     cout << gcd(25, 11) << endl;
     cout << inverse(11, 25) << endl;
+
+    cout << inverse(2, 127) << endl;
+
+
+    cout << endl;
+    cout << "Problem 1.27" << endl;
+
+    int public_key = 391;
+    int encoded_message = (41*41*41 % public_key);
+    cout << encoded_message << endl;
+
+    int secret_decoder = inverse(3, (17 - 1) * (23 - 1));
+    cout << secret_decoder << endl;
+
+    int decoded_message = modexp(encoded_message, secret_decoder, public_key);
+    cout << decoded_message << endl;
+
+    cout << endl;
+    cout << "Problem 1.28" << endl;
+
+    int p = 7;
+    int q = 11;
+    int N = 7 * 11;
+
+    // does the usual `3` work?
+    cout << gcd(3, (p - 1) * (q - 1)) << endl;
+    cout << gcd(7, (p - 1) * (q - 1)) << endl;
+
+    cout << "Public keys: (" << N << ", " << 7 << ")" << endl;
+
+    cout << "Secret key: " << inverse(7, (p - 1) * (q - 1)) << endl;
 }
