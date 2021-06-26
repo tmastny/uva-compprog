63d3697eb7740937d03b5b7657e3de10dda77a74
Timothy Mastny <tim.mastny@gmail.com>
2021-06-26 12:17:05 -0500

    save

---
+++ leetcode/linked-list-odd-even/main.py
@@ -0,0 +1,101 @@
+# Examples:
+#     List             Output
+#     [1,2,3,4,5]      [1,3,5,2,4]
+#     [2,1,3,5,6,4,7]  [2,3,6,7,1,5,4]
+
+# Diagram:
+#   i    0[1,2,3,4,5]
+#   0    e o
+#   1      e o
+#   i    0[1,2,3,4,5]
+#   1    ^   ^
+#          ^   ^
+#            ^   ^
+#              ^   ^
+#   5            ^   ^ <- lead is None, but lag is 4. Then lag.next = evenhead
+#                         doesn't work. Need to use last
+
+#   i    0[1,2,3,4]
+#   1    ^   ^
+#          ^   ^
+#            ^   ^
+#   4          ^   ^ <- lag ends on last odd, lead is None
+
+#   i  0 0[1,2,3,4,5]
+#      ^   ^
+#        ^   ^
+#          ^   ^
+#            ^   ^     while lead.next: // true lead = lead.next; lag = next_lag
+#              ^   ^
+
+
+# Note:
+#   The relative order inside
+#   both the even and odd groups should
+#   remain as it was in the input.
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next: ListNode = next
+
+
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+
+        evenhead = head.next
+
+        lag = ListNode(next=head)
+        lead = head.next
+
+        i = 0
+        while lead:
+            next_lag = lag.next
+            lag.next = lead
+
+            lead = lead.next
+            lag = next_lag
+            i += 1
+
+        last = lag.next
+        lag.next = evenhead if i % 2 == 1 else None
+        last.next = None if i % 2 == 1 else evenhead
+
+        return head
+
+
+def make_node(lst):
+    head = ListNode()
+    node = head
+    for val in lst:
+        node.next = ListNode()
+        node = node.next
+        node.val = val
+
+    return head.next
+
+
+def print_node(head):
+    node = head
+    while node is not None:
+        print(node.val, end=",")
+        node = node.next
+    print()
+
+
+if __name__ == "__main__":
+    cases = [
+        [[1, 2, 3, 4], [1, 3, 2, 4]],
+        [[1, 2, 3, 4, 5], [1, 3, 5, 2, 4]],
+        [[2, 1, 3, 5, 6, 4, 7], [2, 3, 6, 7, 1, 5, 4]],
+    ]
+    cases = [list(map(make_node, case)) for case in cases]
+
+    s = Solution()
+
+    for l, ans in cases:
+        node = s.oddEvenList(l)
+        print_node(node)
