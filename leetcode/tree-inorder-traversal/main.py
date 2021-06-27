aeaac3046609d6c14293bb5739f7908214e03676
Timothy Mastny <tim.mastny@gmail.com>
2021-06-27 10:01:23 -0500

    save

---
+++ leetcode/tree-inorder-traversal/main.py
@@ -0,0 +1,66 @@
+# Example:
+#   Tree Array      Output
+#   [1,null,2,3]    [1,3,2]   <- shouldn't the input be [1,null,2,null,null,3]
+#         1
+#           2
+#         3
+
+from typing import List
+
+
+class TreeNode:
+    @classmethod
+    def _add_to_list(cls, ls, i):
+        if i >= len(ls) or ls[i] is None:
+            return None
+
+        node = cls(ls[i])
+        node.left = cls._add_to_list(ls, i * 2 + 1)
+        node.right = cls._add_to_list(ls, i * 2 + 2)
+
+        return node
+
+    @classmethod
+    def from_list(cls, ls):
+        """
+        [0, 1, 2, 3, 4, 5, 6, 7]
+            0          1
+          1   2      2   3
+         3 4 5 6    4 5 6 7
+        l(0) = 1    l(1) = 3   l(2) = 5   l(i) = i * 2 + 1
+        r(0) = 2    r(1) = 4   r(2) = 6   r(i) = i * 2 + 2s
+        """
+
+        return cls._add_to_list(ls, 0)
+
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def _recursive_traversal(self, node: TreeNode, vals: List[int]):
+        print(node.val)
+        if not node.left:
+            self._recursive_traversal(node.left, vals)
+
+        vals.append(node.val)
+
+        if not node.right:
+            self._recursive_traversal(node.right, vals)
+
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        vals = []
+        self._recursive_traversal(root, vals)
+
+        return vals
+
+
+if __name__ == "__main__":
+    cases = [[1, None, 2, None, None, 3]]
+    cases = [TreeNode.from_list(case) for case in cases]
+
+    s = Solution()
+    for case in cases:
+        print(s.inorderTraversal(case))
