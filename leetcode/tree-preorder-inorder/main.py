cee0fa9ccc1e8f4577d6069441b3f4a9f33bf1d2
Timothy Mastny <tim.mastny@gmail.com>
2021-06-28 20:40:53 -0500

    save

--- leetcode/tree-preorder-inorder/main.py
+++ leetcode/tree-preorder-inorder/main.py
@@ -1,38 +1,15 @@
 # Example:
-#   Input                    Output
-#   [3,9,20,null,null,15,7]  [[3],[20,9],[15,7]]
+#   preorder       inorder         output
+#   [3,9,20,15,7]  [9,3,15,20,7]   [3,9,20,null,null,15,7]
 
-# Looks like a kinda of breadth first search
-import queue
+# Preorder corresponds to a breadth-first search, and iorder
+# is a depth-first search.
+
 from typing import List
 from collections import deque
 
 
 class TreeNode:
-    @classmethod
-    def _add_to_list(cls, ls, i):
-        if i >= len(ls) or ls[i] is None:
-            return None
-
-        node = cls(ls[i])
-        node.left = cls._add_to_list(ls, i * 2 + 1)
-        node.right = cls._add_to_list(ls, i * 2 + 2)
-
-        return node
-
-    @classmethod
-    def from_list(cls, ls):
-        """
-        [0, 1, 2, 3, 4, 5, 6, 7]
-            0          1
-          1   2      2   3
-         3 4 5 6    4 5 6 7
-        l(0) = 1    l(1) = 3   l(2) = 5   l(i) = i * 2 + 1
-        r(0) = 2    r(1) = 4   r(2) = 6   r(i) = i * 2 + 2
-        """
-
-        return cls._add_to_list(ls, 0)
-
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
@@ -41,12 +18,54 @@ class TreeNode:
 
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        return TreeNode()
+        if not inorder or not preorder:
+            return None
+
+        node = TreeNode(preorder[0])
+
+        node_index = inorder.index(preorder[0])
+
+        node.left = self.buildTree(preorder[1:], inorder[0:node_index])
+        node.right = self.buildTree(preorder[2:], inorder[node_index:])
+
+        return node
+
+
+def print_inorder(node):
+    if node is None:
+        return
+
+    print_inorder(node.left)
+    print(node.val, end="")
+    print_inorder(node.right)
+
+
+def print_bfs(node):
+    queue = deque([node])
+    while queue:
+        size = len(queue)
+        for _ in range(size):
+            node = queue.pop()
+            if node is None:
+                continue
+
+            print(node.val, end=",")
+            queue.appendleft(node.left)
+            queue.appendleft(node.right)
 
 
 if __name__ == "__main__":
-    cases = [[[3, 9, 20, 15, 7], [9, 3, 15, 20, 7]], [[-1], [-1]]]
+    cases = [
+        [[1, 2], [1, 2]],
+        [[1, 2], [2, 1]],
+        [[3, 9, 20, 15, 7], [9, 3, 15, 20, 7]],
+        [[-1], [-1]],
+        [[1, 2, 3], [2, 1, 3]],
+    ]
 
     s = Solution()
     for preorder, inorder in cases:
-        print(s.buildTree(preorder, inorder))
+        node = s.buildTree(preorder, inorder)
+        print_inorder(node)
+        print()
+        print_bfs(node)
