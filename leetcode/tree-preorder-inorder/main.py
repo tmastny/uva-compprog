d4deac4a98c18dcbabfc3e0f4057192c431b8763
Timothy Mastny <tim.mastny@gmail.com>
2021-06-29 10:15:47 -0500

    much faster solution

--- leetcode/tree-preorder-inorder/main.py
+++ leetcode/tree-preorder-inorder/main.py
@@ -4,8 +4,18 @@
 
 # See the pre and in order print methods for the definition.
 # Preorder does NOT correspond to a breadth-first search.
+# In fact, they are all a special case of a depth-first search.
+
+# Improvements:
+#   1. build a hash map of the root to the index of the array
+#      so the left/right split can be found in constant time,
+#      instead of a linear search (which makes the algo O(n^2)
+#      instead of O(n)).
+#
+#   2. use indices instead of slices
 
 from typing import List
+
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -29,7 +39,22 @@ class Solution:
         return root
 
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        return self._build(preorder, [0], inorder)
+        val_to_inorder_index = {val: i for i, val in enumerate(inorder)}
+
+        def build(pi, loi, roi):
+            if pi[0] >= len(preorder) or roi - loi <= 0:
+                return None
+
+            node = TreeNode(preorder[pi[0]])
+            ni = val_to_inorder_index[node.val]
+
+            pi[0] += 1
+            node.left = build(pi, loi, ni)
+            node.right = build(pi, ni + 1, roi)
+
+            return node
+
+        return build([0], 0, len(inorder))
 
 
 def print_inorder(node):
