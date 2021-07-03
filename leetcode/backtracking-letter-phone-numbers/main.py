c10e0c0e35863e7eb5d2dfb9b436571e1f3ee9cb
Timothy Mastny <tim.mastny@gmail.com>
2021-07-03 10:36:13 -0500

    save

--- leetcode/backtracking-letter-phone-numbers/main.py
+++ leetcode/backtracking-letter-phone-numbers/main.py
@@ -10,19 +10,18 @@ class Solution:
         "6": "mno",
         "7": "pqrs",
         "8": "tuv",
-        "9": "wzyz",
+        "9": "wxyz",
     }
 
-    def _combo_r(self, digits, combos):
+    def _combo_r(self, prefix, digits, combos):
+        """
+        Speed: 84th percentile
+        """
         if not digits:
-            return ""
+            return combos.append(prefix)
 
-        for i in range(len(digits)):
-            for letter in self.num_to_letters[digits[i]]:
-                for j in range(i + 1, len(digits)):
-                    for letterj in self.num_to_letters[digits[j]]:
-                        combos.append(letter + letterj)
-
+        for letter in self.num_to_letters[digits[0]]:
+            self._combo_r(prefix + letter, digits[1:], combos)
 
     def letterCombinations(self, digits: str) -> List[str]:
         """
@@ -31,19 +30,22 @@ class Solution:
           a       b       c
         d e f   d e f   d e f
         """
-        # if len(digits) == 1:
-        #     return list(self.letterCombinations[digits])
 
         combos = []
-        self._combo_r(digits, combos)
+        if not digits:
+            return combos
+
+        self._combo_r("", digits, combos)
 
         return combos
+
 
 if __name__ == "__main__":
     cases = [
         ["23", ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]],
         ["", []],
         ["2", ["a", "b", "c"]],
+        # ["529", []],
     ]
 
     s = Solution()
