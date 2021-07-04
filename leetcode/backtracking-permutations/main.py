575b517ef48d96c2f2ba7b5afa8664b486cb2ead
Timothy Mastny <tim.mastny@gmail.com>
2021-07-03 19:46:53 -0500

    save

---
+++ leetcode/backtracking-permutations/main.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        pass
+
+
+if __name__ == "__main__":
+    cases = [[1, 2, 3], [0, 1], [1]]
+
+    s = Solution()
+    for n in cases:
+        print(s.permute(n))
