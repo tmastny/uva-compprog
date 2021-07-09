d5272857743af543b9980a6e61a17c01dc8fda7a
Timothy Mastny <tim.mastny@gmail.com>
2021-07-09 16:59:58 -0500

    save

--- leetcode/sort-search-2d-matrix/main.py
+++ leetcode/sort-search-2d-matrix/main.py
@@ -30,61 +30,76 @@ class Solution:
 
         return lo
 
+    def _bmatrix(self, m, target):
+        rlo = clo = 0
+        rhi, chi = len(m), len(m[0])
+
+
+
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         rows = len(matrix)
         cols = len(matrix[0])
 
-        closest_row = max(self._bleft(ColView(matrix, cols // 2), target), rows - 1)
-        closest_col = max(self._bleft(matrix[rows // 2], target), cols - 1)
+        closest_row = min(self._bleft(ColView(matrix, cols // 2), target), rows - 1)
+        closest_col = min(self._bleft(matrix[rows // 2], target), cols - 1)
 
 
-
-        row_on_col = self._bleft(ColView(matrix, closest_col), target)
+        row_on_col = min(self._bleft(ColView(matrix, closest_col), target), rows - 1)
         if matrix[row_on_col][closest_col] == target:
             return True
 
-        col_on_row = self._bleft(matrix[closest_row], target)
+        col_on_row = min(self._bleft(matrix[closest_row], target), cols - 1)
         return matrix[closest_row][col_on_row] == target
 
 
 if __name__ == "__main__":
     cases = [
+        # [
+        #     [
+        #         [1, 4, 7, 11, 15],
+        #         [2, 5, 8, 12, 19],
+        #         [3, 6, 9, 16, 22],
+        #         [10, 13, 14, 17, 24],
+        #         [18, 21, 23, 26, 30],
+        #     ],
+        #     5,
+        # ],
+        # [
+        #     [
+        #         [1, 4, 7, 11, 15],
+        #         [2, 5, 8, 12, 19],
+        #         [3, 6, 9, 16, 22],
+        #         [10, 13, 14, 17, 24],
+        #         [18, 21, 23, 26, 30],
+        #     ],
+        #     20,
+        # ],
+        # [
+        #     [
+        #         [1, 4, 7, 11, 15],
+        #         [2, 5, 8, 12, 19],
+        #         [3, 6, 9, 16, 22],
+        #         [10, 13, 14, 17, 24],
+        #         [18, 21, 23, 26, 30],
+        #     ],
+        #     24,
+        # ],
+        # [[[1, 4, 7, 11, 15]], 15],
+        # [[[1, 4, 7, 11, 15]], 2],
+        # [[[3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 20],
+        # [[[3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 10],
+        # [[[1]], 1],
+        # [[[1]], 5],
         [
             [
-                [1, 4, 7, 11, 15],
-                [2, 5, 8, 12, 19],
-                [3, 6, 9, 16, 22],
-                [10, 13, 14, 17, 24],
-                [18, 21, 23, 26, 30],
+                [1, 2, 3, 4, 5],
+                [6, 7, 8, 9, 10],
+                [11, 12, 13, 14, 15],
+                [16, 17, 18, 19, 20],
+                [21, 22, 23, 24, 25],
             ],
             5,
         ],
-        [
-            [
-                [1, 4, 7, 11, 15],
-                [2, 5, 8, 12, 19],
-                [3, 6, 9, 16, 22],
-                [10, 13, 14, 17, 24],
-                [18, 21, 23, 26, 30],
-            ],
-            20,
-        ],
-                [
-            [
-                [1, 4, 7, 11, 15],
-                [2, 5, 8, 12, 19],
-                [3, 6, 9, 16, 22],
-                [10, 13, 14, 17, 24],
-                [18, 21, 23, 26, 30],
-            ],
-            24,
-        ],
-        [[[1, 4, 7, 11, 15]], 15],
-        [[[1, 4, 7, 11, 15]], 2],
-        [[[3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 20],
-        [[[3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 10],
-        [[[1]], 1],
-        [[[1]], 5],
     ]
 
     s = Solution()
