20cbe68cd79bfec47521bc4eafef4abe567631f4
Timothy Mastny <tim.mastny@gmail.com>
2021-07-10 14:13:05 -0500

    save

--- leetcode/sort-search-2d-matrix/main.py
+++ leetcode/sort-search-2d-matrix/main.py
@@ -5,12 +5,12 @@ from typing import List
 
 
 # Partitions matrix into two submatrices (*, &).
-# if t > v:         if t < v:
-#    v +++++++|       &&&&&++++++|
-#    &&&&&&&&&|       &&&&&++++++|
-#    ---------|       &&&&&v_____|
-#                     &&&&&______|
-#                     -----------|
+# if t > v:          if t < v:
+#     ______&&&&&|       &&&&&++++++|
+#     ______&&&&&|       &&&&&++++++|
+#     _____v&&&&&|       &&&&&v_____|
+#     ++++++&&&&&|       &&&&&______|
+#     -----------|       -----------|
 
 
 class Solution:
@@ -35,23 +35,26 @@ class Solution:
                 return True
 
         else:
-            if self._in_matrix(m, target, (rmid, rmid + 1), (cmid + 1, crange[1])):
+            if self._in_matrix(m, target, (rmid + 1, rrange[1]), (crange[0], cmid + 1)):
                 return True
 
-            if self._in_matrix(m, target, (rmid + 1, rrange[1]), (cmid, crange[1])):
+            if self._in_matrix(m, target, rrange, (cmid + 1, crange[1])):
                 return True
 
         return False
 
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        """
+        Speed 24, memory 10
+        """
         return self._in_matrix(matrix, target, (0, len(matrix)), (0, len(matrix[0])))
 
 
 if __name__ == "__main__":
     cases = [
-        [[[1, 4, 7, 11, 15]], 15],
-        [[[1, 4, 7, 11, 15]], 2],
-        [[[1, 2, 3, 4, 6], [4, 6, 8, 9, 10], [5, 7, 13, 14, 15]], 5],
+        [[[1, 4, 7, 11, 15]], 15, True],
+        [[[1, 4, 7, 11, 15]], 2, False],
+        [[[1, 2, 3, 4, 6], [4, 6, 8, 9, 10], [5, 7, 13, 14, 15]], 5, True],
         [
             [
                 [1, 4, 7, 11, 15],
@@ -60,7 +63,7 @@ if __name__ == "__main__":
                 [10, 13, 14, 17, 24],
                 [18, 21, 23, 26, 30],
             ],
-            5,
+            5, True
         ],
         [
             [
@@ -70,7 +73,7 @@ if __name__ == "__main__":
                 [10, 13, 14, 17, 24],
                 [18, 21, 23, 26, 30],
             ],
-            20,
+            20, False
         ],
         [
             [
@@ -80,12 +83,12 @@ if __name__ == "__main__":
                 [10, 13, 14, 17, 24],
                 [18, 21, 23, 26, 30],
             ],
-            24,
+            24, True
         ],
-        [[[3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 20],
-        [[[3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 10],
-        [[[1]], 1],
-        [[[1]], 5],
+        [[[3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 20, False],
+        [[[3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 10, True],
+        [[[1]], 1, True],
+        [[[1]], 5, False],
         [
             [
                 [1, 2, 3, 4, 5],
@@ -94,10 +97,22 @@ if __name__ == "__main__":
                 [16, 17, 18, 19, 20],
                 [21, 22, 23, 24, 25],
             ],
-            5,
+            5, True
+        ],
+        [[[5, 6, 10, 14], [6, 10, 13, 18], [10, 13, 18, 19]], 14, True],
+        [
+            [
+                [1, 4, 7, 11, 15],
+                [2, 5, 8, 12, 19],
+                [3, 6, 9, 16, 22],
+                [10, 13, 14, 17, 24],
+                [18, 21, 23, 26, 30],
+            ],
+            21, True
         ],
     ]
 
     s = Solution()
-    for matrix, target in cases:
-        print(s.searchMatrix(matrix, target))
+    print("Func  solution")
+    for matrix, target, solution in cases:
+        print(s.searchMatrix(matrix, target), solution)
