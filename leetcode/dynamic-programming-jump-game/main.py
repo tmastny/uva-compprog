cf3cfc32b6be87560fe8808cdf81f1d207e3bbe3
Timothy Mastny <tim.mastny@gmail.com>
2021-07-10 09:34:26 -0500

    save

--- leetcode/dynamic-programming-jump-game/main.py
+++ leetcode/dynamic-programming-jump-game/main.py
@@ -1,13 +1,38 @@
 from typing import List
 
+# The current value of `n` is the number of indices
+# you are allowed to travel moving forward.
+
+# The dynamic programming invariant is the number
+# spaces you are allowed to move forward, which is
+# based on the current value of the iteration
+# and the maximum of the previous (minus the current
+# iteration).
+
+# Example:
+#   n = [3, 1, 0, 0]
+#   i   n[i]     steps
+#   0   3        3
+#   1   1        max(n[i], last_max - 1) = 2
+#   2   0        1
+#   3   -        -
+
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        pass
+
+        steps = 0
+        for i in range(len(nums) - 1):
+            steps = max(nums[i], steps - 1)
+
+            if steps <= 0:
+                return False
+
+        return True
 
 
 if __name__ == "__main__":
-    cases = [[[2, 3, 1, 1, 4], True], [[3, 2, 1, 0, 4], False]]
+    cases = [[[3, 1, 0, 0], True], [[2, 3, 1, 1, 4], True], [[3, 2, 1, 0, 4], False]]
 
     s = Solution()
     for nums, _ in cases:
