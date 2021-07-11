99140eea6da9cdd7d2e3082339ce38e799eee1f8
Timothy Mastny <tim.mastny@gmail.com>
2021-07-11 14:03:56 -0500

    save

--- leetcode/dynamic-programming-coin-change/main.py
+++ leetcode/dynamic-programming-coin-change/main.py
@@ -4,7 +4,18 @@ from typing import List
 
 # Option 1: Greedy
 #   Use the largest denomination of coin until the next coin
-#   would be greater then the amount. Repeat for the largest.
+#   would be greater then the amount. Repeat for the next largest.
+#
+#   If no combination of coins with the largest coin equal the amount,
+#   try again with the next largest.
+#
+#   Counter-example that the greedy solution does not always produce
+#   the minimum:
+#       coins = [2, 3, 9], amount = 10. 1 9, neither 3 nor 2 make 10.
+#       3 3s, 2s do not make 10. Then the greedy approach would return 5 2s.
+#       But this isn't the best solution: 2 3s, 2 2s for a minimum of 4.
+#
+#
 #   Challenges:
 #       Prove that some combination of coins could represent
 #       the amount, the greedy strategy would achieve it.
@@ -23,13 +34,26 @@ from typing import List
 #   coins = [1, 5], amount = 5. The solution to 4 would require
 #   4 coins, while 5 would require 1.
 
+# Option 3: Reminder
+#   Find the reminder for each coin into the amount. This is more
+#   efficient than repeatly adding numbers until the next coin
+#   would be larger than the total. May only amount to an optimization
+#   of option 1.
+
+
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         pass
 
 
 if __name__ == "__main__":
-    cases = [[[1, 2, 5], 11, 3], [[2], 3, -1], [[1], 0, 0], [[1], 1, 1], [[1], 2, 2]]
+    cases = [
+        [[1, 2, 5], 11, 3],
+        [[2], 3, -1],
+        [[1], 0, 0],
+        [[1], 1, 1],
+        [[1], 2, 2][[2, 3, 9], 10, 4],
+    ]
 
     s = Solution()
     for coins, amount, ans in cases:
