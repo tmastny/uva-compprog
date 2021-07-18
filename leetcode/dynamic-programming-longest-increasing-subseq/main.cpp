bfd046b8429728c274b69445443f83547a356b37
Timothy Mastny <tim.mastny@gmail.com>
2021-07-17 23:04:00 -0500

    save

--- leetcode/dynamic-programming-longest-increasing-subseq/main.cpp
+++ leetcode/dynamic-programming-longest-increasing-subseq/main.cpp
@@ -26,23 +26,26 @@ public:
         auto nums_deduped = remove_successive_dups(nums);
 
         map<int, int> RunCounter {{nums_deduped[0], 1}};
-        int prev_run_length = 1;
+        int longest_run = nums_deduped[0];
         for (int i = 1; i < nums_deduped.size(); i++) {
             int n = nums_deduped[i];
 
-            auto it = RunCounter.lower_bound(n);
+            if (n > longest_run) {
+				RunCounter[n] = RunCounter[longest_run] + 1;
+				longest_run = n;
 
-            int run_length = 1;
-            if (it != RunCounter.begin()) {
-                run_length = prev(it)->second + 1;
-            }
+			} else {
+				auto it = RunCounter.lower_bound(n);
+				int run_length = 1;
+				if (it != RunCounter.begin()) {
+					run_length = prev(it)->second + 1;
+				}
 
-            if (n > nums_deduped[i - 1]) {
-                run_length = max(run_length, prev_run_length + 1);
-            }
-
-            RunCounter[n] = run_length;
-            prev_run_length = run_length;
+				RunCounter[n] = run_length;
+				if (run_length > RunCounter[longest_run]) {
+					longest_run = n;
+				}
+			}
         }
 
         int max_run_length = 0;
@@ -58,24 +61,24 @@ public:
 
 int main() {
     vector<tuple<vector<int>, int>> cases = {
-        // {{10, 11, 12, 13, 14, 1, 2, 3}, 5},
-        // {{1, 2, 10, 3, 4}, 4},
-        // {{0, 10, 11, 12, 13, 1, 2, 3, 4, 5}, 6},
-        // {{10, 9, 2, 5, 3, 7, 101, 18}, 4},
-        // {{0, 9, 4, 10, 3, 15, 5, 18, 1, 20}, 6},
-        // {{0, 1, 0, 3, 2, 3}, 4},
-        // {{7, 7, 7, 7, 7, 7, 7}, 1},
+        {{10, 11, 12, 13, 14, 1, 2, 3}, 5},
+        {{1, 2, 10, 3, 4}, 4},
+        {{0, 10, 11, 12, 13, 1, 2, 3, 4, 5}, 6},
+        {{10, 9, 2, 5, 3, 7, 101, 18}, 4},
+        {{0, 9, 4, 10, 3, 15, 5, 18, 1, 20}, 6},
+        {{0, 1, 0, 3, 2, 3}, 4},
+        {{7, 7, 7, 7, 7, 7, 7}, 1},
 
         // have to be careful about how to handle duplicates
-        // {{7, 6, 7, 8}, 3},
-        // {{0, 7, 6, 1, 5, 2, 4, 3}, 4},
-        // {{7, 6, 5, 4, 3, 2, 1, 0}, 1},
-        // {{41, 20, 30, 21, 22, 23, 24, 10}, 5},
-        // {{7, 1, 5, 2, 3, 4, 6, 0}, 5},
-        // {{100, 99, 98, 97, 96, 21, 22, 23, 0, 1}, 3},
-        // {{100, 99, 98, 97, 96, 21, 110, 22, 109, 23, 101, 0, 1}, 4},
-        // {{10, 23, 41, 3, 61}, 4},
-        // {{2, 15, 3, 7, 8, 6, 18}, 5},
+        {{7, 6, 7, 8}, 3},
+        {{0, 7, 6, 1, 5, 2, 4, 3}, 4},
+        {{7, 6, 5, 4, 3, 2, 1, 0}, 1},
+        {{41, 20, 30, 21, 22, 23, 24, 10}, 5},
+        {{7, 1, 5, 2, 3, 4, 6, 0}, 5},
+        {{100, 99, 98, 97, 96, 21, 22, 23, 0, 1}, 3},
+        {{100, 99, 98, 97, 96, 21, 110, 22, 109, 23, 101, 0, 1}, 4},
+        {{10, 23, 41, 3, 61}, 4},
+        {{2, 15, 3, 7, 8, 6, 18}, 5},
 
         // which subsequence should the last element continue?
         // the latest element needs to continue whichever is the longest
@@ -92,16 +95,32 @@ int main() {
         // 80 or 60 are valid continuations
             {{80, 60, 100}, 2},
             {{80, 60, 100, 61, 62}, 3},
-            {{80, 60, 100, 81, 82}, 3}
+            {{80, 60, 100, 81, 82}, 3},
 
-        // {{9, 7, 8, 6, 10, 7, 8, 9}, 4}
+        {{9, 7, 8, 6, 10, 7, 8, 9}, 4}
     };
 
     Solution s;
 
     for (auto&& test : cases) {
-        cout << s.lengthOfLIS(get<0>(test)) << " " << get<1>(test) << endl;
-        //cout << "*******" << endl;
+        int calc = s.lengthOfLIS(get<0>(test));
+		int ans = get<1>(test);
+
+		if (calc != ans) {
+			cout << "Wrong Answer: ";
+		}
+
+		cout << calc << " " << ans << endl;
+        cout << "*******" << endl;
     }
 
 }
+
+		// {{9, 7, 8, 6, 10}, 3},
+
+		//    counter  run
+		// 9   1       1
+		// 7   1       1
+		// 8   2       2 @ 8
+		// 6   1       2 @ 8
+		// 10          3 @ 10
