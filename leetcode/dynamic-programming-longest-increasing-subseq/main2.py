6ab61be9b5870c1e6aba4d6c73a7e154a631686a
Tim Mastny <tim.mastny@gmail.com>
2022-10-21 14:54:40 -0500

    save

--- leetcode/dynamic-programming-longest-increasing-subseq/main2.py
+++ leetcode/dynamic-programming-longest-increasing-subseq/main2.py
@@ -2,23 +2,23 @@ from typing import List
 from collections import deque
 
 cases = [
-    [[11, 10], 1],
-    [[10, 11], 2],
-    [[10, 11, 12, 13, 14, 1, 2, 3], 5],
-    [[1, 2, 10, 3, 4], 4],
+    # [[11, 10], 1],
+    # [[10, 11], 2],
+    # [[10, 11, 12, 13, 14, 1, 2, 3], 5],
+    # [[1, 2, 10, 3, 4], 4],
     [[0, 10, 11, 12, 13, 1, 2, 3, 4, 5], 6],
-    [[10, 9, 2, 5, 3, 7, 101, 18], 4],
-    [[0, 9, 4, 10, 3, 15, 5, 18, 1, 20], 6],
-    [[0, 1, 0, 3, 2, 3], 4],
-    [[7, 7, 7, 7, 7, 7, 7], 1],
-    [[7, 6, 7, 8], 3],  # duplicates
-    [[0, 7, 6, 1, 5, 2, 4, 3], 4],
-    [[7, 6, 5, 4, 3, 2, 1, 0], 1],
-    [[41, 20, 30, 21, 22, 23, 24, 10], 5],
-    [[7, 1, 5, 2, 3, 4, 6, 0], 5],
-    [[100, 99, 98, 97, 96, 21, 22, 23, 0, 1], 3],
-    [[100, 99, 98, 97, 96, 21, 110, 22, 109, 23, 101, 0, 1], 4],
-    [[10, 23, 41, 3, 61], 4],
+    # [[10, 9, 2, 5, 3, 7, 101, 18], 4],
+    # [[0, 9, 4, 10, 3, 15, 5, 18, 1, 20], 6],
+    # [[0, 1, 0, 3, 2, 3], 4],
+    # [[7, 7, 7, 7, 7, 7, 7], 1],
+    # [[7, 6, 7, 8], 3],  # duplicates
+    # [[0, 7, 6, 1, 5, 2, 4, 3], 4],
+    # [[7, 6, 5, 4, 3, 2, 1, 0], 1],
+    # [[41, 20, 30, 21, 22, 23, 24, 10], 5],
+    # [[7, 1, 5, 2, 3, 4, 6, 0], 5],
+    # [[100, 99, 98, 97, 96, 21, 22, 23, 0, 1], 3],
+    # [[100, 99, 98, 97, 96, 21, 110, 22, 109, 23, 101, 0, 1], 4],
+    # [[10, 23, 41, 3, 61], 4],
 ]
 
 
@@ -28,7 +28,9 @@ def print_nums(nums):
     print()
 
 
-class Solution:
+# cached dynamic solution. Extremely slow: either
+# too slow to pass or in the 99th percentile 
+class SolutionSlow:
 
     lenLISAtIndex = []
 
@@ -49,10 +51,55 @@ class Solution:
         self.lenLISAtIndex = [0 for i in range(len(nums))]
 
         for i in range(len(nums)):
-            self.lenLISAtIndex[i] = self._findLenLISAtIndex(nums, i)
+            self._findLenLISAtIndex(nums, i)
 
         return max(self.lenLISAtIndex)
 
+# O(n log n):
+#   in a strictly increasing sequence, each iteration through
+#   nums we add the next element at the end of tails.
+#   Up to that point, the length of tails is the length
+#   of the longest increasing subsequence. 
+
+#   Once we encounter a non-increasing value, 
+#   the length of LIS doesn't increase. But this new
+#   value could be the start of a subsequence that is
+#   longer than the one we are currently tracking. So
+#   we do a binary search to find where the element
+#   would go in tails and replace the current value. 
+#   That way if there is another value larger than the current
+#   max, the length increases. Otherwise, if there are values 
+#   greater than the new one, but less than the max,
+#   we can keep building that sequence until we expand the array.
+class Solution:
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        # tails[j] is the smallest element of `nums`
+        # in a subsequence of length `j`. 
+        tails = [0] * len(nums)
+        
+        # length is the number of non-zero elements in `tails`
+        length = 0
+    
+        # we iterate over each element of `nums` and binary search
+        # for the placement in `tails`    
+        for n in nums:
+            lo, hi = 0, length
+            while lo != hi:
+                mid = (lo + hi) // 2
+                if tails[mid] < n:
+                    lo = mid + 1
+                else:
+                    hi = mid
+            tails[lo] = n
+            length = max(lo + 1, length)
+            
+            print(f'n={n:>2}, lo={lo:>2}, hi={hi:>2}, l={length:>2}, tails={tails}')
+            
+        return length
+
+        
 
 # Find the length of the longest increasing subsequence
 
