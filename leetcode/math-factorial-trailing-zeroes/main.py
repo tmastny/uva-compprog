a4a3771b7523b62aacdface272e2f9952fb08a2d
Tim Mastny <tim.mastny@gmail.com>
2022-10-26 14:01:17 -0500

    save

---
+++ leetcode/math-factorial-trailing-zeroes/main.py
@@ -0,0 +1,31 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        zeroes, twos, fives = 0, 0, 0
+        for i in range(1, n + 1):
+            while i % 2 == 0:
+                twos += 1
+                i //= 2
+            
+            while i % 5 == 0:
+                fives += 1
+                i //= 5            
+            
+        
+        zeroes += min(twos, fives)
+    
+        return zeroes
+        
+cases = [
+    (3, 0),
+    (5, 1),
+    (0, 0)
+]
+if __name__ == "__main__":
+    for input, ans in cases:
+        s = Solution()
+        output = s.trailingZeroes(input)
+        
+        if output != ans:
+            print(f'input={input}, output={output}, ans={ans}')
+            
+    
