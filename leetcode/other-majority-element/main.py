8ae7f436d51b9e15dd9b6b22eecce4620b5f019c
Tim Mastny <tim.mastny@wave.com>
2022-11-04 20:48:02 -0500

    save

--- leetcode/other-majority-element/main.py
+++ leetcode/other-majority-element/main.py
@@ -18,6 +18,33 @@ class SolutionDict:
         return max(counter, key=counter.get)
 
 
+# O(n log(n)) speed, O(1) memory. Sort and calculate longest run
+# 58% speed, 0% memory
+class SolutionSort:
+    def majorityElement(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        nums.sort()
+
+        max_run = 0
+        max_el = None
+        run = 1
+        for i in range(0, len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                run += 1
+            else:
+                if run > max_run:
+                    max_el = nums[i]
+                    max_run = run
+                run = 1
+
+        if run > max_run:
+            max_el = nums[i]
+
+        return max_el
+
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         counter = defaultdict(int)
@@ -28,6 +55,7 @@ class Solution:
 
 
 cases = [
+    ([6, 6, 6, 7, 7], 6),
     ([3, 2, 3], 3),
     ([2, 2, 1, 1, 1, 2, 2], 2),
 ]
