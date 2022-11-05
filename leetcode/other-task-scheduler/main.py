7b815fc29d2581d0c48c74f04732b145f0745431
Tim Mastny <tim.mastny@wave.com>
2022-11-05 17:56:41 -0500

    save

--- leetcode/other-task-scheduler/main.py
+++ leetcode/other-task-scheduler/main.py
@@ -4,16 +4,20 @@ from collections import Counter
 # edge case where you want to prioritize/start the most common
 # so you aren't bottlenecked at the end waiting on one task
 
-
+# This solution does not garauntee that the tasks are executed
+# in the *least* amount of time. It only finds a solution that
+# respects all the task delays.
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         delay = n + 1
 
+        # orders task from most to least common
         num_tasks = Counter(tasks)
         tasks = {task: {"num": num, "delay": 0} for task, num in num_tasks.items()}
 
         time = 0
         while tasks:
+            # finds a task with no delay to execute
             for task, stats in tasks.items():
                 if stats["delay"] == 0:
                     stats["delay"] = delay
@@ -35,6 +39,7 @@ cases = [
     (["A", "A", "A", "B", "B", "B"], 2, 8),
     (["A", "A", "A", "B", "B", "B"], 0, 6),
     (["A", "A", "A", "A", "A", "A", "B", "C", "D", "E", "F", "G"], 2, 16),
+    (["A", "A", "A", "B", "B", "B", "C", "C", "C", "D", "D", "E"], 2, 12),
 ]
 
 if __name__ == "__main__":
