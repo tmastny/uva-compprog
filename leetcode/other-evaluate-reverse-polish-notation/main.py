415d4e9004c11d9083ec18903a12e19fe2025339
Tim Mastny <tim.mastny@wave.com>
2022-11-04 20:19:12 -0500

    save

--- leetcode/other-evaluate-reverse-polish-notation/main.py
+++ leetcode/other-evaluate-reverse-polish-notation/main.py
@@ -1,15 +1,30 @@
-from random import randint
-from sys import argv
-
+from operator import add, mul, sub
+from typing import List
 
 class Solution:
-    def evalRPN(self, tokens: List[str]) -> int:
-        return 0
+   operators = {"+": add, "-": sub, "*": mul, "/": lambda l, r: int(l / r)}
+
+   def evalRPN(self, tokens: List[str]) -> int:
+        tokens = tokens
+        stack = []
+
+        for t in tokens:
+            if t not in self.operators:
+                stack.append(int(t))
+                continue
+
+
+            func = self.operators[t]
+            right = stack.pop()
+            left = stack.pop()
+            stack.append(func(left, right))
+
+        return stack.pop()
 
 
 cases = [
-    (["2", "1", "+", "3", "*"], 9),
-    (["4", "13", "5", "/", "+"], 6),
+    # (["2", "1", "+", "3", "*"], 9),
+    # (["4", "13", "5", "/", "+"], 6),
     (["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"], 22),
 ]
 
