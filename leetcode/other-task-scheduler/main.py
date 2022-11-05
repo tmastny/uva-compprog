ebb78c29f044ffc36d805e2b72b3d65b342cb601
Tim Mastny <tim.mastny@wave.com>
2022-11-05 18:16:09 -0500

    save

--- leetcode/other-task-scheduler/main.py
+++ leetcode/other-task-scheduler/main.py
@@ -35,25 +35,27 @@ class SolutionFinishTasks:
         return time
 
 
+# Greedy approach: the next task should always be one with no delay,
+# but the largest number of remaining executions
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         delay = n + 1
 
-        # orders task from most to least common
         num_tasks = Counter(tasks)
         tasks = {task: {"num": num, "delay": 0} for task, num in num_tasks.items()}
 
         time = 0
         while tasks:
-            # finds a task with no delay to execute
-            for task, stats in tasks.items():
-                if stats["delay"] == 0:
-                    stats["delay"] = delay
 
-                    tasks[task]["num"] -= 1
-                    if tasks[task]["num"] == 0:
-                        del tasks[task]
-                    break
+            available = [task for task, stats in tasks.items() if stats["delay"] == 0]
+            if available:
+                # task with most remaining num
+                next = max(available, key=lambda t: tasks[t]["num"])
+                tasks[next]["delay"] = delay
+
+                tasks[next]["num"] -= 1
+                if tasks[next]["num"] == 0:
+                    del tasks[next]
 
             time += 1
             for task, stats in tasks.items():
